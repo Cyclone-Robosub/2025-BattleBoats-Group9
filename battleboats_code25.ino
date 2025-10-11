6af881468bc1fe8f1a9d5caa6f54cfcf5e88ee1e
Matthew Lin <linmatthew2007@gmail.com>
2025-10-11 12:18:40 -0700

    Remove old drive code

--- battleboats_code25.ino
+++ battleboats_code25.ino
@@ -8,16 +8,12 @@ void setup() {
 }
 
 void loop() {
-  float dist = readDistanceCM();
+  
   Serial.print("Distance: ");
   Serial.print(dist);
   Serial.println(" cm");
 
-  if (dist > 15) {
-    driveForward(150);
-  } else {
-    driveStop();
-  }
+
 
   delay(100);
 }
